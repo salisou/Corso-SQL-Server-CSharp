a481592fca1dd6754ae66a586f0cac0561c6cb0c
Salisou <salisoumoussa1@gmail.com>
2026-04-24 09:35:13 +0200

    add new features queries

---
+++ 24_04_2026.sql
@@ -0,0 +1,23 @@
+/*
+	Funzioni aggregate SQL
+		SUM, AVG, COUNT, MIN e MAX permettono calcoli su gruppi di dati in SQL Server.
+
+	Having: 
+		SELECT 
+			....
+		FROM ....
+		GROUP BY ...
+		HAVING condizione
+
+	JOIN / INNER JOIN
+	LEFT JOIN -> mostra tutti i dati della tabella a sinistra e quelli corrispondenti della tabella a destra, se non ci sono corrispondenze mostra NULL
+	REIGHT JOIN -> mostra tutti i dati della tabella a destra e quelli corrispondenti della tabella a sinistra, se non ci sono corrispondenze mostra NULL
+	
+	group by
+	order by
+
+	Alter Tabele
+	Alter column
+
+	Update e Delete
+*/
