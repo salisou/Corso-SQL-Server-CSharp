e41c738229bc9e39657250a7301872a24237628a
Salisou <salisoumoussa1@gmail.com>
2026-04-24 13:03:16 +0200

    Update quesries

--- 24_04_2026.sql
+++ 24_04_2026.sql
@@ -116,3 +116,182 @@ SELECT
 FROM Studenti s
 LEFT JOIN Iscrizioni i ON i.StudenteId = s.StudenteId
 LEFT JOIN Corsi c ON c.CorsoId = i.CorsoId
+
+
+-- COUNT CON JOIN
+-- Contare gli studenti per coso(Left Join, GROUP by)
+SELECT
+	NomeCorso,
+FROM Corsi c       
+LEFT JOIN
+GROUP BY
+
+
+select * from Studenti;
+
+select 
+	Nome
+from Studenti;
+
+
+select 
+	Nome,
+	Cognome
+from Studenti;
+
+select 
+	Nome,
+	Cognome,
+	Email
+from Studenti;
+
+select 
+	Nome,
+	Cognome,
+	Email,
+	CodiceFiscale
+from Studenti;
+
+select 
+	Nome,
+	Cognome,
+	Email,
+	CodiceFiscale
+from Studenti
+ORDER BY Nome DESC
+
+select 
+	Nome,
+	Cognome,
+	Email,
+	CodiceFiscale
+from Studenti
+ORDER BY Nome ASC
+
+
+select 
+	COUNT(*) AS 'Numero righe' -- PER CONTARE LE RIGHE E AS = Aliass
+from Studenti;
+
+select 
+	 *
+from Studenti
+WHERE StudenteId = 2;
+
+
+select 
+	 *
+from Studenti
+WHERE StudenteId = 2 OR StudenteId = 20;
+
+SELECT 
+ * 
+FROM Studenti
+WHERE Nome = 'Mario' OR Nome = 'Luca' OR Nome = 'Rossi'
+ORDER BY Nome asc
+
+-- IN(1,2,3...) = PER LA LISTA
+SELECT 
+ * 
+FROM Studenti
+WHERE StudenteId IN (1,25,30,4,8,3,10,7)
+ORDER BY StudenteId asc
+
+select 
+	 *
+from Studenti
+WHERE Nome = 'Mario' 
+
+
+-- like '....%'
+-- like '%...'
+-- like '&....%'
+select 
+	 *
+from Studenti
+WHERE Nome like 'M%';
+
+select 
+	 *
+from Studenti
+WHERE Cognome like '%i'
+
+select 
+	 *
+from Studenti
+WHERE Nome like '%a'
+
+
+select 
+	 *
+from Studenti
+WHERE Nome like '%o%'
+
+
+select 
+	 *
+from Corsi
+WHERE NomeCorso like 'P%' AND DescrizioneCorso is null
+
+
+select 
+	 *
+from Corsi
+WHERE NomeCorso like 'P%' AND DescrizioneCorso is NOT null
+
+select 
+	 TOP 5 *
+from Corsi
+ORDER BY NomeCorso asc
+
+-- gli operatori
+-- somma +
+SELECT 
+	Crediti + 2 as creditiAumentati
+FROM Corsi;
+
+-- * / %
+select 
+	Crediti * 2 as Doppio,
+	Crediti / 2 as Meta,
+	Crediti % 2 as Resto
+from Corsi;
+
+-- CONCATENAZIONE (stringhe)
+-- in SQL server i usa + 
+select 
+	Nome + ' - ' + Cognome AS 'Nome completo'
+from Studenti;
+
+select 
+	*
+from Studenti s
+INNER JOIN Iscrizioni i
+	ON i.StudenteId = s.StudenteId -- On Usa l'operatore di confronto =
+where s.Nome like 'A%';
+
+SELECT *
+FROM Studenti
+WHERE (Nome like 'M%' OR Nome like 'L%') 
+	  AND DataNascita >= '2000' 
+	  AND Email is not null
+-- Alter table
+/*
+	ALTER TABLE NOME DELLA TABLLA
+	ADD NOME DELLA COLONNA CON IL TIPO 
+*/
+ALTER TABLE Studenti
+ADD Genere CHAR(1); -- M = Maschio, F = Femmina
+
+SELECT * FROM Studenti;
+
+-- ALTER COLUMN -> per modificare 
+ALTER TABLE Studenti
+ALTER COLUMN Nome NVARCHAR(100);
+
+
+-- Eliminare la colonna Genere 
+ALTER TABLE Studenti
+DROP COLUMN Genere 
+
+-- Update  Delete con nstore procedure
